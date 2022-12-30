274d2e6d33ba5575945cfb39ce279fc0d99365b8
luquilo <luquilo.etc@gmail.com>
2022-12-30 23:55:42 +0700

    lha

--- index.js
+++ index.js
@@ -91,4 +91,4 @@ app.listen(port, () => {
 // ini ada di sesi6
 
 //ini adalah percobaan update di branch sesi 6
-console.log('halo dari branch 6 tes ganti')
+console.log('halo dari branch 6 tes gantii')
